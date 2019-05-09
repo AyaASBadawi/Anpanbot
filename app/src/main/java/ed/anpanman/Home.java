59b2ecfae6713a3cb11ed04b97006a59145e48b4
BananEmad <33826865+BananEmad@users.noreply.github.com>
2019-05-09 19:15:46 +0200

    3

--- app/src/main/java/ed/anpanman/Home.java
+++ app/src/main/java/ed/anpanman/Home.java
@@ -2,7 +2,7 @@ package ed.anpanman;
 
 import android.content.Intent;
 import android.graphics.Rect;
-import android.provider.Telephony;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,26 +14,38 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.nightonke.boommenu.ButtonEnum;
 import com.nightonke.boommenu.Util;
+import ed.anpanman.Model.Student;
 
 public class Home extends AppCompatActivity {
     TextView nameuser, walletuser, review, network, plugins, myapps, mainmenus,
             pagetitle, pagesubtitle;
     Button btnguide;
     Animation atg, atg3, atg4;
-    ImageView imageView3,chat,qb,quiz,lessons;
+    ImageView imageView3,chat,qb,quiz,lessons,profilepic;
     BoomMenuButton bmb ;
+    FirebaseUser firebaseUser;
+    DatabaseReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         nameuser =(TextView) findViewById(R.id.nameuser);
-
+        profilepic=(ImageView)findViewById(R.id.imageView2);
         review = (TextView)findViewById(R.id.review);
         network =(TextView) findViewById(R.id.network);
         plugins = (TextView)findViewById(R.id.plugins);
@@ -46,14 +58,41 @@ public class Home extends AppCompatActivity {
         qb= (ImageView)findViewById(R.id.imgqb);
         quiz= (ImageView)findViewById(R.id.imgquiz);
         lessons= (ImageView)findViewById(R.id.imglessons);
+//firebase
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        ref= FirebaseDatabase.getInstance().getReference("Student").child(firebaseUser.getUid());
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Student student=dataSnapshot.getValue(Student.class);
+                nameuser.setText(student.getUsername());
+                if(student.getImageURL().equals("default"))
+                {
+
+                    profilepic.setImageResource(R.drawable.userpic);
+                }
+                else
+                {
+
+                    Glide.with(Home.this).load(student.getImageURL()).into(profilepic);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 //boom menu
         bmb = (BoomMenuButton) findViewById(R.id.bmb);
 
         bmb.setButtonEnum(ButtonEnum.TextOutsideCircle);
         for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
-            int posiion =i;
-          if (posiion==0)
+            int position =i;
+          if (position==0)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_chat)
@@ -73,13 +112,14 @@ public class Home extends AppCompatActivity {
                           public void onBoomButtonClick(int index) {
                               // When the boom-button corresponding this builder is clicked.
                               Intent it = new Intent(Home.this, chat.class);
+                              it.putExtra("userid","l5eZsfdMjNXXw2wMyat4rEDz4SH3");
                               startActivity(it);
                           }
                       });
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==1)
+          else if (position==1)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_profile)
@@ -104,7 +144,7 @@ public class Home extends AppCompatActivity {
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==2)
+          else if (position==2)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_quiz)
@@ -129,7 +169,7 @@ public class Home extends AppCompatActivity {
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==3)
+          else if (position==3)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_bq)
@@ -154,7 +194,7 @@ public class Home extends AppCompatActivity {
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==4)
+          else if (position==4)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_lessons)
@@ -179,7 +219,7 @@ public class Home extends AppCompatActivity {
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==5)
+          else if (position==5)
 
          {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
@@ -206,13 +246,13 @@ public class Home extends AppCompatActivity {
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==6)
+          else if (position==6)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
-                      .normalImageRes(R.drawable.ic_login)
+                      .normalImageRes(R.drawable.ic_log_out)
                       .rotateImage(false)
                       .rotateText(false)
-                      .normalTextRes(R.string.login)
+                      .normalTextRes(R.string.logout)
                       .shadowEffect(true)
                       .shadowOffsetX(20)
                       .shadowOffsetY(0)
@@ -223,15 +263,15 @@ public class Home extends AppCompatActivity {
                       .listener(new OnBMClickListener() {
                           @Override
                           public void onBoomButtonClick(int index) {
-                              // When the boom-button corresponding this builder is clicked.
-                              Intent it = new Intent(Home.this, login.class);
+                              FirebaseAuth.getInstance().signOut();
+                              Intent it = new Intent(Home.this, parent.class);
                               startActivity(it);
                           }
                       });
 
               bmb.addBuilder(builder);
           }
-          else if (posiion==7)
+          else if (position==7)
           {
               TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
                       .normalImageRes(R.drawable.ic_rateus)
@@ -269,15 +309,16 @@ public class Home extends AppCompatActivity {
 
         }
 
-chat.setOnClickListener(new View.OnClickListener()
-{
+         chat.setOnClickListener(new View.OnClickListener()
+             {
     @Override
     public void onClick(View v)
     {
         Intent it = new Intent(Home.this, chat.class);
+        it.putExtra("userid","l5eZsfdMjNXXw2wMyat4rEDz4SH3");
         startActivity(it);
-    }
-});
+                       }
+                         });
         qb.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -312,19 +353,6 @@ chat.setOnClickListener(new View.OnClickListener()
         atg3 = AnimationUtils.loadAnimation(this, R.anim.atg3);
         atg4 = AnimationUtils.loadAnimation(this, R.anim.atg4);
 
-      /*  Typeface MLight=Typeface.createFromAsset(getAssets(),"fonts/Ml.ttf");
-        Typeface MMedium=Typeface.createFromAsset(getAssets(),"fonts/MM.ttf");
-        Typeface MRegular=Typeface.createFromAsset(getAssets(),"fonts/MR.ttf");
-        nameuser.setTypeface(MMedium);
-        walletuser.setTypeface(MLight);
-        mainmenus.setTypeface(MRegular);
-        review.setTypeface(MMedium);
-        network.setTypeface(MMedium);
-        plugins.setTypeface(MMedium);
-        myapps.setTypeface(MMedium);
-        btnguide.setTypeface(MMedium);
-        pagetitle.setTypeface(MRegular);
-        pagesubtitle.setTypeface(MLight);*/
 
         // pass an animation
         imageView3.startAnimation(atg);
--- app/src/main/java/ed/anpanman/chat.java
+++ app/src/main/java/ed/anpanman/chat.java
@@ -1,13 +1,150 @@
 package ed.anpanman;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import ed.anpanman.Adapter.MessageAdapter;
+import ed.anpanman.Model.Chat;
+import ed.anpanman.Model.Student;
 
 public class chat extends AppCompatActivity {
 
+
+
+    CircleImageView profile_image;
+
+    ImageButton btn_mic,btn_send;
+    TextView mess,username;
+
+
+    MessageAdapter messageAdapter;
+    List<Chat>mchat;
+    RecyclerView recyclerView;
+
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        mess = (TextView) findViewById(R.id.txt_message);
+        btn_mic = (ImageButton) findViewById(R.id.btn_mic);
+        btn_send = (ImageButton) findViewById(R.id.btn_send);
+        profile_image = (CircleImageView) findViewById(R.id.profile_image);
+        username = (TextView) findViewById(R.id.username);
+
+        intent = getIntent();
+        final String userid = intent.getStringExtra("userid");
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mess.equals("")) {
+                    String msg = mess.getText().toString().trim();
+                    SendMessage(fuser.getUid(), userid, msg);
+
+                } else {
+                    Toast.makeText(chat.this, "لا يمكن ارسال رساله فارغه !!", Toast.LENGTH_LONG).show();
+                }
+                mess.setText("");
+            }
+        });
+
+
+        reference = FirebaseDatabase.getInstance().getReference("Student").child(userid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Student student = dataSnapshot.getValue(Student.class);
+                username.setText(student.getUsername());
+                if (student.getImageURL().equals("default")) {
+                    profile_image.setImageResource(R.drawable.userpic);
+                } else {
+                    Glide.with(chat.this).load(student.getImageURL());
+                }
+                readMesagges(fuser.getUid(),userid,"default");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+
+    private void SendMessage(String sender, String reciever,String message)
+    {
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
+        HashMap<String,Object> hashMap=new HashMap<>();
+        hashMap.put("Sender",sender);
+        hashMap.put("Receiver",reciever);
+        hashMap.put("Message",message);
+        ref.child("Chat").push().setValue(hashMap);
+
+    }
+    private void readMesagges(final String myid, final String userid, final String imageurl){
+        mchat = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mchat.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat chat = snapshot.getValue(Chat.class);
+                    if (chat.getReceiver().equals(myid) && chat.getSender().equals(userid) ||
+                            chat.getReceiver().equals(userid) && chat.getSender().equals(myid)){
+                        mchat.add(chat);
+                    }
+
+                    messageAdapter = new MessageAdapter(chat.this, mchat, imageurl);
+                    recyclerView.setAdapter(messageAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
